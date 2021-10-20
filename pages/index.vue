2d03172637aa981535e7b0f8d77401d6a0d433b5
LuanVasco <devluangodoy@gmail.com>
2021-10-20 00:05:41 -0300

    arrumando filstro por body

--- pages/index.vue
+++ pages/index.vue
@@ -15,19 +15,36 @@ export default {
     return {}  
   },
   async asyncData({ $axios, store }) {
+    var raw = JSON.stringify({
+      "query": {
+        "bool": {
+          "must": [
+            { "match": { "estabelecimento_municipio_nome": "RIBEIRAO PRETO"} },
+            { "match": { "paciente_enumSexoBiologico": "M" }},
+            { "match": { "vacina_descricao_dose": "1ª Dose" }}
+          ]
+        }
+      }
+    });
+
     const params = {
-      auth: {
-        username: 'imunizacao_public',
-        password: 'qlto5t&7r_@+#Tlstigi'
+      method: 'POST',
+      body: raw,
+      headers: {
+        "Authorization": "Basic aW11bml6YWNhb19wdWJsaWM6cWx0bzV0JjdyX0ArI1Rsc3RpZ2k=",
+        "Content-Type": "application/json"
       },
+      redirect: 'follow',
       params: {
-        size: 1000
-      },
+        size: 1000,
+      }
     }
-    const { hits } = await $axios.$get('https://imunizacao-es.saude.gov.br/_search?scroll=1m', params)
-    let data = hits.hits
-    store.commit('setDados', data)
-    return { data }
+    
+    const response = await fetch("https://imunizacao-es.saude.gov.br/_search?scroll=1m", params).then(res => res.json());
+    
+    store.commit('setDados', response)
+
+    return response
   },
 }
 </script>
